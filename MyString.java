e69d47aa43e1c432081388ed7f64644a2222b8b1
unknown <matan.cutler@runi.post.ac.il>
2025-11-22 00:41:31 +0200

    ex 1-3 complete

--- MyString.java
+++ MyString.java
@@ -19,12 +19,40 @@ public class MyString {
     /** Returns the lowercase version of the given string. */
     public static String lowerCase(String str) {
         // Replace the following statement with your code
-        return null;
+        String Nstring = "";
+        for(int i = 0; i < str.length(); i ++)
+        {
+            if(str.charAt(i) >= 65 && str.charAt(i) <= 90)
+                Nstring += (char) (str.charAt(i) + 32);
+            else
+                Nstring += str.charAt(i);
+        }
+        return Nstring;
     }
 
     /** If str1 contains str2, returns true; otherwise returns false. */
     public static boolean contains(String str1, String str2) {
         // Replace the following statement with your code
+        String NewStr1 = str1;
+        String NewStr2 = str2;
+        int counter = 0;
+        if(NewStr2.length() > NewStr1.length() || NewStr1.length() == 0) return false;
+        if(NewStr2.length() == 0) return true;
+        for (int i = 0; i < NewStr1.length(); i ++)
+        {
+            if (str1.charAt(i) == NewStr2.charAt(0))
+            {
+                for (int j = i; j < NewStr1.length(); j ++)
+                {
+                    if(NewStr1.charAt(j) != NewStr2.charAt(counter))
+                        break;
+                    if (counter == NewStr2.length()-1) 
+                        return true;
+                    counter ++;
+                }
+                counter = 0;
+            }
+        }
         return false;
     }
 }
--- Primes.java
+++ Primes.java
@@ -1,5 +1,24 @@
 public class Primes {
     public static void main(String[] args) {
         // Replace this statement with your code
+        int n = Integer.parseInt(args[0]);
+        boolean [] prime = new boolean [n + 1];
+        double sum = 0;
+        int counter = 0;
+        for (int i = 0; i < prime.length; i++) prime[i] = true;
+        prime[0] = false;
+        prime[1] = false;
+        prime[2] = true;
+        for(int p = 2; p*p <=n; p++)
+        {
+            if(prime[p] == true)
+            {
+                for(int i = p*p; i <= n; i+=p)
+                    prime[i] = false;
+            }
+        }
+        for (int i = 0; i < prime.length; i++) if(prime[i] == true) counter++;
+        sum = ((double) counter/n)*100;
+        System.out.println("There are " + counter + " primes between 2 and " + n + " (" + (int) sum +"% are primes)");
     }
 }
